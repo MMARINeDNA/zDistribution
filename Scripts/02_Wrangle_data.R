77f0d4211c7412f2e8538dd99973ba212f5297ff
Eiren Jacobson <eiren.jacobson@gmail.com>
2025-01-22 11:42:09 +0000

    Data wrangled into detections/nondetections for each sample, primer, dilution, species, collapsed across tech reps.

--- Scripts/02_Wrangle_data.R
+++ Scripts/02_Wrangle_data.R
@@ -1,32 +1,80 @@
 #### 3D Distribution
-#### wrangling data
+#### wrangling data for presence/absence modelling
 #### January 2025
 #### EKJ
 
 library(tidyverse)
-library(mgcv)
 
-load("./ProcessedData/detect_data_meta.RData")
+# First, load data and keep records from all samples, even if no cetaceans detected
 
-# need to expand to 1/0 for each possible species+sample combo
+metadata <- read.csv("./Data/Hake_2019_metadata.csv")
 
-all_species_samples <- detect_data_meta %>%
-  expand(nesting(Sample_name, primer, dilution, techRep, station, depth, volume, 
-                 bottom.depth.consensus, utm.lat, utm.lon), BestTaxon) 
+detect_list <- list.files(path = "./Data", pattern = "taxon_table.csv", 
+                          recursive = TRUE, full.names = TRUE)
 
-nondetections <- all_species_samples %>% 
-  anti_join(detect_data_meta) %>%
-  mutate(Detected = 0)
+data_samples <- readr::read_csv(detect_list, id = "file_name") %>%
+  select(-BestTaxon, -Class, -nReads) %>%
+  unique()
 
-detections <- detect_data_meta %>%
-  select(names(all_species_samples)) %>%
-  mutate(Detected = 1)
+# these are all of the samples that were run, with SampleUID as the unique identifier
+samples_info <- data_samples %>% 
+  separate(Sample_name, 
+           into = c("primer","NWFSCpopID","NWFSCsampNum","dilution","techRep"),
+           sep = "-",
+           remove = FALSE) %>% 
+  filter(NWFSCpopID == 52193) %>% 
+  unite(NWFSCsampleID, NWFSCpopID:NWFSCsampNum, sep = "-") %>% 
+  separate(techRep, into = c("techRep",NA), sep = "_") %>% 
+  separate(file_name, into = c(NA,NA,"data",NA,NA), sep = "/") %>% 
+  separate(data, into = c(NA,"Plate",NA,NA), sep = "_")  %>%
+  mutate(SampleUID = paste0(Sample_name, "_", Plate))
 
-binary_data <- rbind.data.frame(detections, nondetections)
+# okay, now want a record for each possible species x sample
 
-# now want to collapse across tech reps, so we have one record per sample
+samples_info_species <- expand_grid(SampleUID = data_samples_info$SampleUID, 
+                                    BestTaxon = unique(detect_data$BestTaxon)) %>%
+  left_join(samples_info, by = "SampleUID")
 
+# now want to fill in whether each record resulted in a detection or not
 
+# first, modify AVC code to not include metadata yet, just detections
+detect_data_nometa <- detect_data %>% 
+  separate(Sample_name, 
+           into = c("primer","NWFSCpopID","NWFSCsampNum","dilution","techRep"),
+           sep = "-",
+           remove = FALSE) %>% 
+  filter(NWFSCpopID == 52193) %>% 
+  unite(NWFSCsampleID, NWFSCpopID:NWFSCsampNum, sep = "-") %>% 
+  separate(techRep, into = c("techRep",NA), sep = "_") %>% 
+  separate(file_name, into = c(NA,NA,"data",NA,NA), sep = "/") %>% 
+  separate(data, into = c(NA,"Plate",NA,NA), sep = "_") %>%
+  mutate(SampleUID = paste0(Sample_name, "_", Plate))
+
+# binary data for all possible sample x species combos
+
+binary_detect_species <- left_join(samples_info_species, detect_data_nometa,
+                                   by = c("Plate", "Sample_name", "primer", 
+                                          "NWFSCsampleID", "dilution", "techRep", 
+                                          "BestTaxon", "SampleUID")) %>%
+  select(-Class) %>%
+  replace_na(list(nReads = 0)) %>%
+  mutate(Detected = ifelse(nReads>0, 1, 0)) %>%
+  filter(techRep != "control" & techRep != "d1") %>%
+  mutate(techRep = as.numeric(techRep))
+
+# now want to collapse across techReps so that nTechReps is max(techRep)
+
+detect_species_techreps <- binary_detect_species %>%
+  group_by(BestTaxon, Plate, primer, NWFSCsampleID, dilution) %>%
+  summarize(nTechReps = max(techRep), nReads = max(nReads), Detected = max(Detected))
+
+# okay now we're ready to attach metadata
+
+detect_species_meta <- left_join(detect_species_techreps, metadata, by = c("NWFSCsampleID" = "sampleID"))
+
+# save the file and we're done!
+
+save(detect_species_meta, file = "./ProcessedData/detect_species_meta.RData")
 
          
          
