0d7523485df07158e934cefc34fceae264b4a3ef
Amy Van Cise <avancise@gmail.com>
2025-01-31 12:23:28 -0800

    Combine data formatting code, fix issues with sample nomenclature, add time-at-depth data, remove duplicative code, separate figure code into a new script: AVC 1/31/25

--- Scripts/01.1_Plot_detections.R
+++ Scripts/01.1_Plot_detections.R
@@ -1,8 +1,35 @@
+### 3D distribution
+### Density distribution by depth
+### January 2025
+### AVC
 
-# Requires running 01_Read_format_data.R
-# to generate object detect_by_station
+#### Set up environment --------------------------------------------------------
 
-ggplot(detect_by_station, aes(y = BestTaxon, x = depth, 
+library(tidyverse)
+library(PNWColors)
+library(ggridges)
+
+load("./ProcessedData/detect_species_meta.RData")
+metadata <- read.csv("./Data/Hake_2019_metadata.csv")
+
+#### Add metadata --------------------------------------------------------------
+
+detect_data_meta <- detect_data %>% 
+  left_join(metadata, by = c("NWFSCsampleID" = "sampleID"))
+
+#### Collapse by station/species -----------------------------------------------
+
+detect_by_station <- detect_data_meta %>% 
+  group_by(station, depth, techRep, BestTaxon) %>% 
+  mutate(totReads = sum(nReads)) %>% 
+  mutate(detect = case_when(totReads > 1 ~ 1,
+                            TRUE ~ 0)) %>% 
+  slice_head() %>% 
+  ungroup() %>% 
+  filter(detect == 1)
+
+#### Bubbleplot ----------------------------------------------------------------
+detectDepth_bubble <- ggplot(detect_by_station, aes(y = BestTaxon, x = depth, 
                               fill = BestTaxon, color = BestTaxon)) +
   geom_count() +
   theme_minimal() + 
@@ -14,3 +41,42 @@ ggplot(detect_by_station, aes(y = BestTaxon, x = depth,
   scale_color_manual(values = c(pnw_palette("Cascades",12, type = "continuous"),
                                 pnw_palette("Sunset",12, type = "continuous")[1:11])) +
   theme(legend.position = "none")
+
+#### Ridgeplot -----------------------------------------------------------------
+
+lowdetect_subset <- detect_by_station %>% 
+  group_by(BestTaxon) %>% 
+  mutate(nDetect = n()) %>% 
+  filter(nDetect < 3)
+
+detectDepth_ridge <- ggplot(detect_by_station, aes(y = BestTaxon, x = depth, 
+                              fill = BestTaxon, color = BestTaxon)) +
+  geom_density_ridges(scale = 1, 
+                      bandwidth = 100, 
+                      jittered_points = TRUE,
+                      point_alpha = 1,
+                      point_shape = 21,
+                      alpha = 0.6) +
+  geom_point(data = lowdetect_subset, aes()) +
+  theme_minimal() + 
+  coord_flip() +
+  scale_x_reverse() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  scale_fill_manual(values = c(pnw_palette("Cascades",12, type = "continuous"),
+                               pnw_palette("Sunset",12, type = "continuous")[1:12])) +
+  scale_color_manual(values = c(pnw_palette("Cascades",12, type = "continuous"),
+                                pnw_palette("Sunset",12, type = "continuous")[1:12])) +
+  theme(legend.position = "none")
+
+#### Save figures -------------------------------------------------------------
+
+save(detectDepth_bubble, detectDepth_ridge, file = "./ProcessedData/detectDepth_plots.Rdata")
+
+pdf(file = "./Figures/detectDepth_bubble.pdf")
+detectDepth_bubble
+dev.off()
+
+pdf(file = "./Figures/detectDepth_ridge.pdf")
+detectDepth_ridge
+dev.off()
+
---
+++ Scripts/01_Wrangle_data.R
@@ -0,0 +1,114 @@
+#### 3D Distribution
+#### wrangling data for presence/absence modelling
+#### January 2025
+#### EKJ&AVC
+
+library(tidyverse)
+
+# Load metadata
+metadata <- read.csv("./Data/Hake_2019_metadata.csv")
+timeAtDepth <- read.csv("./Data/MM_dive_time_expand.csv")
+mmEcoEvo <- read.csv("./Data/MM_metadata.csv")
+
+# get all detections
+detect_list <- list.files(path = "./Data", pattern = "taxon_table.csv", 
+                          recursive = TRUE, full.names = TRUE)
+
+detect_data <- readr::read_csv(detect_list, id = "file_name") %>% 
+  filter(Class == "Mammalia") %>% 
+  filter(!(BestTaxon %in% c("Homo sapiens", "Bos taurus", 
+                            "Sus scrofa", "Bos",
+                            "Ovis", "Felis catus",
+                            "Artiodactyla","Canis lupus",
+                            "Macropodidae", "Diprotodontia",
+                            "Macropus fuliginosus",
+                            "Macropus giganteus",
+                            "Osphranter","Osphranter robustus",
+                            "Osphranter rufus", "Dama dama",
+                            "Canis", "Felis", "Petrogale xanthopus", 
+                            "Homo", "Tursiops truncatus"))) %>% 
+  mutate(BestTaxon = case_when(BestTaxon == "Lagenorhynchus" ~ "Lagenorhynchus obliquidens",
+                               TRUE ~ BestTaxon)) %>% 
+  separate(Sample_name, 
+           into = c("primer","NWFSCpopID","NWFSCsampNum","dilution","techRep"),
+           sep = "-",
+           remove = FALSE) %>% 
+  filter(NWFSCpopID == 52193) %>% 
+  unite(NWFSCsampleID, NWFSCpopID:NWFSCsampNum, sep = "-") %>% 
+  separate(techRep, into = c("techRep",NA), sep = "_") %>% 
+  separate(file_name, into = c(NA,NA,"data",NA,NA), sep = "/") %>% 
+  separate(data, into = c(NA,"Plate",NA,NA), sep = "_") %>%
+  mutate(SampleUID = paste0(Sample_name, "_", Plate))
+
+# All samples, even if no cetaceans detected
+data_samples <- readr::read_csv(detect_list, id = "file_name") %>%
+  select(-BestTaxon, -Class, -nReads) %>%
+  unique()
+
+# these are all of the samples that were run, with SampleUID as the unique identifier
+samples_info <- data_samples %>% 
+  separate(Sample_name, 
+           into = c("primer","NWFSCpopID","NWFSCsampNum","dilution","techRep"),
+           sep = "-",
+           remove = FALSE) %>% 
+  filter(NWFSCpopID == 52193) %>% 
+  unite(NWFSCsampleID, NWFSCpopID:NWFSCsampNum, sep = "-") %>% 
+  separate(techRep, into = c("techRep",NA), sep = "_") %>% 
+  separate(file_name, into = c(NA,NA,"data",NA,NA), sep = "/") %>% 
+  separate(data, into = c(NA,"Plate",NA,NA), sep = "_")  %>%
+  mutate(SampleUID = paste0(Sample_name, "_", Plate))
+
+# okay, now want a record for each possible species x sample
+
+samples_info_species <- expand_grid(SampleUID = samples_info$SampleUID, 
+                                    BestTaxon = unique(detect_data$BestTaxon)) %>%
+  left_join(samples_info, by = "SampleUID")
+
+# now want to fill in whether each record resulted in a detection or not
+# binary data for all possible sample x species combos
+
+binary_detect_species <- left_join(samples_info_species, detect_data,
+                                   by = c("Plate", "Sample_name", "primer", 
+                                          "NWFSCsampleID", "dilution", "techRep", 
+                                          "BestTaxon", "SampleUID")) %>%
+  select(-Class) %>%
+  replace_na(list(nReads = 0)) %>%
+  mutate(Detected = ifelse(nReads>0, 1, 0)) %>%
+  mutate(techRep1 = techRep, .after = techRep) %>% 
+  mutate(techRep = ifelse(Plate == 313 | Sample_name == "MV1-52193-460-5-d10-1_S39", dilution, techRep)) %>% 
+  mutate(dilution = ifelse(Plate == 313 | Sample_name == "MV1-52193-460-5-d10-1_S39", techRep1, dilution)) %>% 
+  filter(!grepl("control", Sample_name)) %>% 
+  select(-techRep1) %>% 
+  mutate(techRep = as.numeric(techRep))
+
+# now want to collapse across techReps so that nTechReps is max(techRep)
+
+detect_species_techreps <- binary_detect_species %>%
+  group_by(BestTaxon, Plate, primer, NWFSCsampleID, dilution) %>%
+  summarize(nTechReps = max(techRep), nReads = max(nReads), Detected = max(Detected))
+
+# okay now we're ready to attach metadata
+detect_species_meta <- left_join(detect_species_techreps, metadata, 
+                                 by = c("NWFSCsampleID" = "sampleID")) %>% 
+  left_join(mmEcoEvo, by = c("BestTaxon" = "Species"))
+
+# max dive depth by species, per the Navy reports
+maxDepth_species <- timeAtDepth %>% 
+  group_by(Species) %>% 
+  summarize(maxDepth = max(depth))
+
+detect_species_divetime <- detect_species_meta %>% 
+  filter(!(BestTaxon %in% c("Balaenoptera", "Delphinidae"))) %>% 
+  mutate(common_name = case_when(common_name == "killer whale"~"mammal eating killer whale",
+                               TRUE~common_name)) %>% 
+  left_join(timeAtDepth, by = c("common_name" = "Species", "depth" = "depth")) %>% 
+  left_join(maxDepth_species, by = c("common_name" = "Species")) %>% 
+  mutate(time_per_m = case_when(depth > maxDepth~0,
+                                TRUE~time_per_m))
+  
+# save the file and we're done!
+save(detect_data, detect_species_meta, detect_species_divetime,
+     file = "./ProcessedData/detect_species_meta.RData")
+
+         
+         
